ec2e1adc0c3e3b06a09e08767a7de4ce87d025b3
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-04-26 14:12:32 +0300

    Expose help/version gettext helpers; bump to 0.2.0
    Extract gettext strings into module-level get_help_message and get_version_message functions and export them; add static accessor methods on HelpI18nMixin. Add typing.Optional and accept an optional version parameter in the mixin constructor. Update help/version argument wiring to use the new helpers. Regenerate/replace locale .po/.mo files and bump package version to 0.2.0 in pyproject.toml.

--- argparse_help_i18n/__init__.py
+++ argparse_help_i18n/__init__.py
@@ -1,20 +1,31 @@
 import locale
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Optional
 from locale_plus import Internationalizator
 
-__all__ = ['HelpI18nMixin']
+__all__ = ['HelpI18nMixin', 'get_help_message', 'get_version_message']
 
 locale.setlocale(locale.LC_ALL, '')
 gettext = Internationalizator(Path(__file__).parent / 'locale', domain='argparse-help').gettext
 
 
+def get_help_message(): return gettext("Show this help message and exit")
+
+def get_version_message(): return gettext("Show program's version and exit")
+
+
 class HelpI18nMixin(ArgumentParser):
-    def __init__(self, *args, add_help: bool = True, add_version: bool = True, **kwargs):
+    @staticmethod
+    def get_help_message(): return get_help_message()
+
+    @staticmethod
+    def get_version_message(): return get_version_message()
+
+    def __init__(self, *args, add_help: bool = True, add_version: bool = True,
+                 version: Optional[str] = None, **kwargs):
         super().__init__(*args, add_help=False, **kwargs)
         if add_help:
-            self.add_argument('--help', '-h', action='help',
-                              help=gettext("Show this help message and exit"))
+            self.add_argument('--help', '-h', action='help', help=get_help_message())
         if add_version:
-            self.add_argument('--version', '-v', action='version',
-                              help=gettext("Show program's version and exit"))
+            self.add_argument('--version', '-v', action='version', help=get_version_message())
